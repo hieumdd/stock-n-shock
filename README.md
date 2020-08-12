7f687b16a1bd6b394e7164dfafe4231d610f031b
Hieu Mai <hieu.mai@vuanem.com>
2020-08-12 15:12:04 +0700

    Updated README & main file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# stock-n-shock
+# cafef.vn all csv downloader
+## python scrape_cafef_vn.py -s <start_date> -e <end_date>
+Check output @ csv/
--- scrape_cafef_vn.py
+++ scrape_cafef_vn.py
@@ -6,7 +6,25 @@ import zipfile
 import io
 import datetime
 from tqdm import tqdm
+import argparse
+import logging
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-s', '--start',
+                    type=str,
+                    help='Start Date',required=True)
+
+parser.add_argument('-e', '--end',
+                    type=str,
+                    help='End Date', required=True)
+
+args = parser.parse_args()
+start_date = args.start
+end_date = args.end
+
+print(start_date)
+print(end_date)
 def get_all_csv(url):
     with requests.get(url) as r:
         z = zipfile.ZipFile(io.BytesIO(r.content))
@@ -14,7 +32,7 @@ def get_all_csv(url):
 
 base_url = 'http://images1.cafef.vn/data/{0}/CafeF.SolieuGD.{1}.zip'
 
-date_ranges = pd.date_range('2020-01-01', '2020-07-31')
+date_ranges = pd.date_range(start_date, end_date)
 
 def to_1st_date_ranges(x):
     return x.strftime('%Y%m%d')
@@ -26,11 +44,18 @@ date_ranges_1 = np.vectorize(to_1st_date_ranges)(date_ranges.date)
 date_ranges_2 = np.vectorize(to_2nd_date_ranges)(date_ranges.date)
 
 bad_date_ranges = []
-for i in tqdm(np.arange(0,3)):
+
+logging.basicConfig(filename='bad_date_ranges.log', level=logging.INFO)
+
+for i in tqdm(np.arange(0,len(date_ranges))):
     url = base_url.format(date_ranges_1[i], date_ranges_2[i])
     print(url)
     try:
         get_all_csv(url)
     except:
         print(f'Error at {url}')
-        bad_date_ranges.append((date_ranges_1[i], date_ranges_2[i]))
+        bad_date_ranges.append((date_ranges[i]))
+        logging.info(date_ranges_1[i])
+
+bad_date_ranges_df = pd.DataFrame(bad_date_ranges)
+bad_date_ranges_df.to_csv('bad_date_ranges.csv', index=False)
